6bc2652f9cd1fdc825062c8c19c5b0e0c77a461e
Levi Fuller <Levi Fuller>
2017-01-05 00:10:04 -0700

    Added Angular Material, flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { AddPetComponent } from './pages/add-pet/add-pet.component';
 import { BrowsePetsComponent } from './pages/browse-pets/browse-pets.component';
 import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   declarations: [
@@ -18,9 +19,25 @@ import { MaterialModule } from '@angular/material';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    FlexLayoutModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+  //  "@angular/common": "~2.1.0",
+  //   "@angular/compiler": "~2.1.0",
+  //   "@angular/core": "~2.1.0",
+  //   "@angular/forms": "~2.1.0",
+  //   "@angular/http": "~2.1.0",
+  //   "@angular/material": "^2.0.0-beta.1",
+  //   "@angular/platform-browser": "~2.1.0",
+  //   "@angular/platform-browser-dynamic": "~2.1.0",
+  //   "@angular/router": "~3.1.0",
+  //   "core-js": "^2.4.1",
+  //   "rxjs": "5.0.0-beta.12",
+ 
+  //   "zone.js": "^0.6.23"
