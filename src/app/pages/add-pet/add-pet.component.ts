93daddd76eb7d32b6ddbcb00d1c10bc8b1ee176a
Levi <lfuller941@gmail.com>
2017-01-23 17:13:53 -0700

    Refactored image resizing to image.service

--- src/app/pages/add-pet/add-pet.component.ts
+++ src/app/pages/add-pet/add-pet.component.ts
@@ -3,12 +3,13 @@ import { ImgurService } from '../../services/imgur.service';
 import { PetService } from '../../services/pet.service';
 import { Pet, Breed, Animal } from '../../models/models';
 import { Observable } from 'rxjs';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-add-pet',
   templateUrl: './add-pet.component.html',
   styleUrls: ['./add-pet.component.css'],
-  providers: [ImgurService]
+  providers: [ImgurService, ImageService]
 })
 export class AddPetComponent implements OnInit {
 
@@ -16,7 +17,9 @@ export class AddPetComponent implements OnInit {
 
 
   animals = [1, 2, 3, 4, 1, 1, 1, 1, 1];
-  constructor(private _petService: PetService, private _imgurService: ImgurService, private _zone: NgZone) { }
+  constructor(private _petService: PetService, 
+    private _imageService: ImageService,
+    private _imgurService: ImgurService, private _zone: NgZone) { }
   animalSet = [];
   ngOnInit() {
     this._petService.getAnimals().subscribe(animals => this.animalOptions = animals);
@@ -39,85 +42,11 @@ export class AddPetComponent implements OnInit {
     let files: File[] = event.srcElement.files;
     if (files.length > 0) {
       this.imageFile = files[0];
-      let fr = new FileReader();
-      this._zone.run(() => {
-        fr.onload = (frEvent) => {
-          var image = new Image();
-          image.onload = (imageEvent) => {
-            let resizedUrl = this.resize(image, 300, 300);
-            this.pet.image = resizedUrl;
-            console.log(resizedUrl);
-            let resizedImage = this.dataURLToBlob(resizedUrl);
-            console.log(resizedImage);
-            this._imgurService.uploadBlob(resizedImage).subscribe(link => console.log(link));
-          };
-          let frResult = (frEvent as any).target.result;
-          image.src = frResult;
-         // this.pet.image = frResult;
-         // console.log(this.imageFile);
-          
-
-        }
+      this._imageService.getResizedImage(files[0]).subscribe(imageBlob => {
+        console.log(imageBlob);
+        this.pet.image = imageBlob.data;
       });
-      fr.readAsDataURL(this.imageFile)
     }
-  }
-
-  resize(img, maxWidth: number, maxHeight: number): string {
-    // Get the images current width and height
-    let width = img.width;
-    let height = img.height;
-
-    // Set the WxH to fit the Max values (but maintain proportions)
-    if (width > height) {
-        if (width > maxWidth) {
-            height *= maxWidth / width;
-            width = maxWidth;
-        }
-    } else {
-        if (height > maxHeight) {
-            width *= maxHeight / height;
-            height = maxHeight;
-        }
-    }
-
-    // create a canvas object
-    let canvas = document.createElement("canvas");
-
-    // Set the canvas to the new calculated dimensions
-    canvas.width = width;
-    canvas.height = height;
-    let ctx = canvas.getContext("2d");  
-
-    ctx.drawImage(img, 0, 0,  width, height); 
-
-    // Get this encoded as a jpeg
-    // IMPORTANT: 'jpeg' NOT 'jpg'
-    let dataUrl = canvas.toDataURL('image/jpeg');
-    return dataUrl;
-  }
-
-  dataURLToBlob(dataURL) {
-    let BASE64_MARKER = ';base64,';
-    if (dataURL.indexOf(BASE64_MARKER) == -1) {
-        let parts = dataURL.split(',');
-        let contentType = parts[0].split(':')[1];
-        let raw = parts[1];
-        return new Blob([raw], {type: contentType});
-    }
-
-    let parts = dataURL.split(BASE64_MARKER);
-    let contentType = parts[0].split(':')[1];
-    let raw = window.atob(parts[1]);
-    let rawLength = raw.length;
-
-    let uInt8Array = new Uint8Array(rawLength);
-
-    for (let i = 0; i < rawLength; ++i) {
-        uInt8Array[i] = raw.charCodeAt(i);
-    }
-
-    return new Blob([uInt8Array], {type: contentType});
   }
 
   log(event) {
