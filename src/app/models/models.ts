ccc2a36ef6fc6e6001a1069e2e3fd08c6a5206f2
Levi Fuller <Levi Fuller>
2017-01-24 22:32:08 -0700

    Updated model

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -2,26 +2,11 @@ export interface BaseModel {
     id?: number;
 }
 
-export enum Size {
-    Unspecified = 0,
-    Small = 1,
-    Medium = 2,
-    Large = 3
-}
-
-export enum AnimalType {
-    Unspecified = 0,
-    Dog = 1,
-    Cat = 2
-}
-
 export interface Pet extends BaseModel {
     name?: string;
-    size?: Size;
     birthday?: Date;
-    animalType?: AnimalType;
-    breeds?: string[];
-    image?: string;
+    breeds?: Breed[];
+    imageUrl?: string;
 }
 
 export interface Breed extends BaseModel {
